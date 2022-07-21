84d24aba0f22734915e95b7e1743815ffd320bec
JubyKim <uon9816@naver.com>
2022-07-21 10:49:33 +0900

    swiftui self test

---
+++ swiftSelfTest/swiftSelfTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftSelfTest
+//
+//  Created by JUEUN KIM on 2022/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Let me show off my bong").padding()
+        
+        
+    }
+    
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftSelfTest/swiftSelfTest/swiftSelfTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftSelfTestApp.swift
+//  swiftSelfTest
+//
+//  Created by JUEUN KIM on 2022/07/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftSelfTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
