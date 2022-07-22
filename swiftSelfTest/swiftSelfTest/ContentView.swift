8d4847070a2a85a182ad3d4d6f581e72a5ab4844
JubyKim <uon9816@naver.com>
2022-07-22 10:47:12 +0900

    let me show off my bong

--- swiftSelfTest/swiftSelfTest/ContentView.swift
+++ swiftSelfTest/swiftSelfTest/ContentView.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Let me show off my bong").padding()
+        Text("Let me show off my bong")
+        Divider()
+        ScrollView(.horizontal) {
+            HStack {
+                Image("bong1 복사본 1").resizable().frame(width: 320.0, height: 320.0)
+                Image("bong1 복사본 2").resizable().frame(width: 320.0, height: 320.0)
+                Image("bong1 복사본 3").resizable().frame(width: 320.0, height: 320.0)
+                Image("bong1 복사본 4").resizable().frame(width: 320.0, height: 320.0)
+                Image("bong1 복사본 5").resizable().frame(width: 320.0, height: 320.0)
+                Image("bong1 복사본 6").resizable().frame(width: 320.0, height: 320.0)
+            }
+        }.frame(width: 320.0, height: 320.0)
         
-        
+        ScrollView(.vertical) {
+            Image("bong1 복사본 6").resizable().frame(width: 320.0, height: 320.0)
+            Image("bong1 복사본 6").resizable().frame(width: 320.0, height: 320.0)
+            Image("bong1 복사본 6").resizable().frame(width: 320.0, height: 320.0)
+        }.frame(maxWidth: .infinity)
     }
     
     
