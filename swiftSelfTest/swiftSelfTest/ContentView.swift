3972e8a32683f313b19f9327b73cbab7ac096770
JubyKim <uon9816@naver.com>
2022-07-25 09:26:45 +0900

    show off my dog;

--- swiftSelfTest/swiftSelfTest/ContentView.swift
+++ swiftSelfTest/swiftSelfTest/ContentView.swift
@@ -9,31 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Let me show off my bong")
-        Divider()
-        ScrollView(.horizontal) {
-            HStack {
+        VStack {
+            
+            Text("Let me show off my bong").font(.custom("LeeSeoyun", size: 30))
+            Divider()
+            Text("This is horizontal Scroll").font(.custom("LeeSeoyun", size: 20))
+            ScrollView(.horizontal) {
+                HStack {
+                    Image("bong1 복사본 1").resizable().frame(width: 320.0, height: 320.0)
+                    Image("bong1 복사본 1").resizable().frame(width: 320.0, height: 320.0)
+                    Image("bong1 복사본 1").resizable().frame(width: 320.0, height: 320.0)
+                    Image("bong1 복사본 1").resizable().frame(width: 320.0, height: 320.0)
+                    Image("bong1 복사본 1").resizable().frame(width: 320.0, height: 320.0)
+
+                }
+            }.frame(width: 320.0, height: 320.0)
+            Text("This is vertical Scroll").font(.custom("LeeSeoyun", size: 20))
+            ScrollView(.vertical) {
                 Image("bong1 복사본 1").resizable().frame(width: 320.0, height: 320.0)
-                Image("bong1 복사본 2").resizable().frame(width: 320.0, height: 320.0)
-                Image("bong1 복사본 3").resizable().frame(width: 320.0, height: 320.0)
-                Image("bong1 복사본 4").resizable().frame(width: 320.0, height: 320.0)
-                Image("bong1 복사본 5").resizable().frame(width: 320.0, height: 320.0)
-                Image("bong1 복사본 6").resizable().frame(width: 320.0, height: 320.0)
-            }
-        }.frame(width: 320.0, height: 320.0)
+                Image("bong1 복사본 1").resizable().frame(width: 320.0, height: 320.0)
+                Image("bong1 복사본 1").resizable().frame(width: 320.0, height: 320.0)
+                Image("bong1 복사본 1").resizable().frame(width: 320.0, height: 320.0)
+                Image("bong1 복사본 1").resizable().frame(width: 320.0, height: 320.0)
+            }.frame(width: 320.0, height: 320.0)
+            
+        }
         
-        ScrollView(.vertical) {
-            Image("bong1 복사본 6").resizable().frame(width: 320.0, height: 320.0)
-            Image("bong1 복사본 6").resizable().frame(width: 320.0, height: 320.0)
-            Image("bong1 복사본 6").resizable().frame(width: 320.0, height: 320.0)
-        }.frame(maxWidth: .infinity)
+        
     }
-    
     
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
+    
+    
 }
